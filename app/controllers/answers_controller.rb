ee49af3f8e009f26d66b397094d0da6b94dee559
Austin Kincaid & lindsay culver <student@epicodus.com>
2015-10-19 16:10:59 -0700

    Add delete functionality for users to delete their own answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,14 +5,21 @@ class AnswersController < ApplicationController
   end
 
   def create
-    @answer = Answer.new(answer_params)
     @question = Question.find(params[:question_id])
+    @answer = @question.answers.new(answer_params)
+    @answer.user = current_user
     if @answer.save
-      @question.answers.push(@answer)
-      redirect_to home_index_path
+      redirect_to question_path(@question)
     else
       render :new
     end
+  end
+
+  def destroy
+    @question = Question.find(params[:question_id])
+    @answer = Answer.find(params[:id])
+    @answer.destroy
+    redirect_to question_path(@question)
   end
 
   private
