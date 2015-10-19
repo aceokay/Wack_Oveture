5a389e5d20d22b5002e76dd73c5132a3d83230c1
Austin Kincaid & lindsay culver <student@epicodus.com>
2015-10-19 11:56:45 -0700

    Add specs for answer model associations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ActiveRecord::Base
   has_and_belongs_to_many :tags
   belongs_to :users
+  has_many :answers
   validates :title, :presence => true
   validates :body, :presence => true
 end
