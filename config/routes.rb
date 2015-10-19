902c7e40e928263bfd92598b65951f226df2823e
Austin Kincaid & lindsay culver <student@epicodus.com>
2015-10-19 10:06:19 -0700

    Add sessions controller and new form for logging users in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
+  # is this a good idea? V
+  get "/log-in" => "sessions#new"
+  post "/log-in" => "sessions#create"
+  get "/log-out" => "sessions#destroy", as: :log_out
 
   resources :home
   resources :users
