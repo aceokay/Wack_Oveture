18a12958fd1f9591c6fe32a86712ca10a42a9882
Austin Kincaid & Lindsay Culver <student@epicodus.com>
2015-10-20 10:12:43 -0700

    Add SimpleCov and begin full test coverage

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 describe User do
-  it { should validate_presence_of :name }
-  it { should validate_presence_of :email }
   it { should have_many(:questions) }
   it { should have_many(:answers) }
   it { should have_many(:comments) }
+  it { should validate_confirmation_of :password }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :email }
+  it { should validate_uniqueness_of :email }
+  # it { should use_before_action :encrypt_password }
 end
